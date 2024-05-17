933602508b5fd2b96c998b00e3759173b70fafef
Suetone Carneiro <suetoneneto12@gmail.com>
2024-05-17 16:08:30 -0300

    feat: usuario implementation

--- Code Challenges/POO/01_Criando uma classe de usuario/usuario.py
+++ Code Challenges/POO/01_Criando uma classe de usuario/usuario.py
@@ -1,11 +1,21 @@
-# TODO: Crie uma classe UsuarioTelefone.  
-# TODO: Defina um método especial `__init__`, que é o construtor da classe.
-# O método `__init__`, irá inicializar os atributos da classe: `nome`, `numero` e `plano`.
-
-
-        
-    # TODO: Aplique o conceito de encapsulamento, onde os atributos serão encapsulados dentro da classe.
-      
+class UsuarioTelefone:
+    def __init__(self, nome, numero, plano):
+        self._nome = nome
+        self._numero = numero
+        self._plano = plano
+     
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def numero(self):
+        return self._nome
+    
+    @property
+    def plano(self):
+        return self._plano
+    
 
     # A classe `UsuarioTelefone` define um método especial `__str__`, que retorna uma representação em string do objeto.
     def __str__(self):
@@ -16,6 +26,7 @@
 nome = input()  
 numero = input()  
 plano = input()  
-# TODO: Crie um novo objeto `UsuarioTelefone` com os dados fornecidos:
+
+usuario = UsuarioTelefone(nome, numero, plano)
 
 print(usuario)
